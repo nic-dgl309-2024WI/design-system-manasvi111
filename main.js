2bd95c30c72ec5ee23e54db89dffc857a700a328
manasvi111 <122574665+manasvi111@users.noreply.github.com>
2024-03-24 09:37:51 -0700

    Change the class name in js to make function work

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" , () => {
+document.querySelectorAll(".mainNav__links").forEach(n => n.addEventListener("click" , () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
